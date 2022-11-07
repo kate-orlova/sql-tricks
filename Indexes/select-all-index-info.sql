af229da567584b3ac870b3648eadbe9d99ff43a1
Kate Orlova <kate.s.orlova@gmail.com>
2022-11-07 17:55:48 +0000

    Get info about all indexes at once

---
+++ Indexes/select-all-index-info.sql
@@ -0,0 +1,5 @@
+SELECT a.index_id, name, avg_fragmentation_in_percent, fill_factor, *
+FROM sys.dm_db_index_physical_stats (NULL,NULL, NULL, NULL, NULL) AS a
+JOIN sys.indexes AS b
+ON a.object_id = b.object_id AND a.index_id = b.index_id;
+GO
