f4a85770223028a64d3a0680ae25e779496c0ba0
Kate Orlova <kate.s.orlova@gmail.com>
2023-04-10 21:00:52 +0100

    Select long-running or uncommitted transactions

---
+++ Transactions/select_long-running_uncommitted_transactions.sql
@@ -0,0 +1,8 @@
+SELECT [s_tst].[session_id],
+[database_name] = DB_NAME (s_tdt.database_id),
+[s_tdt].[database_transaction_begin_time], 
+[sql_text] = [s_est].[text] 
+FROM sys.dm_tran_database_transactions [s_tdt]
+INNER JOIN sys.dm_tran_session_transactions [s_tst] ON [s_tst].[transaction_id] = [s_tdt].[transaction_id]
+INNER JOIN sys.dm_exec_connections [s_ec] ON [s_ec].[session_id] = [s_tst].[session_id]
+CROSS APPLY sys.dm_exec_sql_text ([s_ec].[most_recent_sql_handle]) AS [s_est];
